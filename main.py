ad88c888cf75f1976b360cc97f06811c6797611b
mahdiwork <165424460+mahdiwork@users.noreply.github.com>
2024-05-22 11:48:49 +0330

    Update main.py17

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import amar
 import checkpay
 
 print("ok")
-database2.create_database()
+#database2.create_database()
 # database2.insert_users(56464564)
 # from nltk.corpus import wordnet
 # import nltk
@@ -53,7 +53,7 @@ channel_video=-1002146375544
 channel_maghale=-1002028086764
 
 
-admin=748626808#120389165
+admin=120389165
 channel_id= -1001898964360
 channel1_id = -1002016755212  # Replace with your channel1 ID
 channel2_id = -1001992750806  # Replace with your channel2 ID
